8d5cc839064e6cc373df3475adbe1047ffbf28c1
klinek <konrad.linek@gmail.com>
2021-11-23 22:30:38 +0100

    Development server setup - by webpack plugin serve

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,29 @@ const { mode } = require("webpack-nano/argv");
 const {
     MiniHtmlWebpackPlugin,
 } = require("mini-html-webpack-plugin");
+const { WebpackPluginServe: Serve } = require("webpack-plugin-serve");
 
 module.exports = {
+    watch: mode == "development",
+    entry: ["./src", "webpack-plugin-serve/client"],
+    stats: {
+        colors: true,
+        modules: true,
+        reasons: true,
+        errorDetails: true
+    },
     mode,
+    
     plugins: [
-        new MiniHtmlWebpackPlugin({ context: { title: "Demo"} })
-    ]
+        new MiniHtmlWebpackPlugin({ context: { title: "Demo"} }),
+        new Serve({
+            host: "127.0.0.1",
+            port: process.env.PORT || 8080,
+            static: "./dist",
+            // open: true,
+            liveReload: true,
+            waitForBuild: true,
+            historyFallback: true,
+        }),
+    ],
 };
